0dc735bbb18babd66c254493a1647d3bd7b7156a
FreakyCoder <kurayogun@gmail.com>
2019-10-22 12:18:24 +0300

    TSlint, TSConfig & Build files are integrated

--- example.ts
+++
@@ -1,6 +0,0 @@
-import PermissionService from "./index";
-
-const permission: string = "permission.system.project.*.delete_button.3";
-const permissionObj = PermissionService.generatePermissionObj(permission);
-
-console.log("PermissionObj: ", permissionObj);
