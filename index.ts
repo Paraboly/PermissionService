36c2244e185d1f3f5decccf3fd3841d5f7bbe6f2
FreakyCoder <kurayogun@gmail.com>
2019-10-22 11:52:48 +0300

    GeneratePermissionObject and generatePermissionEncode functions are integrated

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ class PermissionService {
   encodedPermission: string = null;
   permissionObj: Object = {};
 
+  /**
+   * @param (string) encoded permission
+   * ? Encoded Permission Param Example:
+   * ? "permission.system.project.*.delete_button.3"
+   */
   generatePermissionObj = (permission: string) => {
     // Split permissions to get permission levels
     const permissionLevels = permission.split(".");
@@ -21,6 +26,28 @@ class PermissionService {
     permissionObj[module][page][component][componentId] = permissionValue;
     return permissionObj;
   };
+
+  /**
+   * @param {string} namespace
+   * @param {string} module
+   * @param {string} page
+   * @param {string} component
+   * @param {string} component_id
+   * @param {number} permission // ? This should be an integer !
+   *
+   * ? Example Encoded Permission:
+   * ? "permission.system.project.*.delete_button.3"
+   */
+  generatePermissionEncode(
+    namespace: string = "permission",
+    module: string,
+    page: string,
+    component: string = "*",
+    component_id: string = "*",
+    permission: number = 0
+  ) {
+    return `[${namespace}][${module}][${page}][${component}][${component_id}][${permission}]`;
+  }
 }
 
 export default new PermissionService();
